f6abdfd5ff08dc1495776820ab53e4bcaf225fb5
Alexey Rogachev <arogachev90@gmail.com>
2018-05-13 15:58:00 +0600

    Replicated project on stackblitz.com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TestComponent } from './test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, TestComponent ],
+  bootstrap:    [ AppComponent ],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
